f17e4043ae42f70488f858958073332d897a459c
galaxyplay1234 <galaxyplay1234@gmail.com>
2026-09-05 09:24:12 -0300

    Atualizar o games.js

--- api/games.js
+++ api/games.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   try {
 
     const MASTER_IPS = [
-      "177.54.84.181",
+      "177.54.84.42",
       "177.54.84.181",
       "177.54.93.93",
       "177.75.110.183",
